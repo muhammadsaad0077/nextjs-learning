0a8f03f2bdd7c727471102b3ff572d5dcc3e5f86
muhammadsaad0077 <saaditanoli12@gmail.com>
2025-03-12 21:39:48 +0500

    project-setup

---
+++ app/components/Hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+function Hello(){
+    console.log("I am client component");
+    
+    return(
+        <div>
+            <h1>Hello hello</h1>
+        </div>
+    )
+}
+
+export default Hello;
